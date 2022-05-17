d43e270ded18646bdc832526d16f08744f28b93a
patison5 <patison4@yandex.ru>
2022-05-17 09:37:41 -0400

    подправил вывод на экран

--- lab1/main.py
+++ lab1/main.py
@@ -6,11 +6,17 @@ if __name__ == '__main__':
     cache.set('Jesse', 'Pinkman')
     cache.set('Walter', 'White')
     cache.set('Jesse', 'James')
-    print("вернёт 'James': ", cache.get('Jesse'))  # вернёт 'James'
-    cache.rem('Walter')
-    print("вернёт 'key not found!': ", cache.get('Walter'))  # вернёт 'key not found!'
 
-    print("~ Элементы двусвязного списка:")
+    print('\n~ Весь набор элементов::')
+    cache.list_all_map_items()
+    
+    print("\nОбратимся по ключу 'Jesse', ожидаемый результат:  'James': ", cache.get('Jesse'))  # вернёт 'James'
+
+    print("\nУдалим значение по ключу Walter")
+    cache.rem('Walter')
+    print("Обратимся по ключу Walter, ожидаемый результат 'key not found!': ", cache.get('Walter'))  # вернёт 'key not found!'
+
+    print("\n~ Элементы двусвязного списка:")
     cache.list_all_items()
 
     print('\n~ Мапа:')
